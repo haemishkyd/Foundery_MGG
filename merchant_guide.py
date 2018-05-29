f1d5eb54a8546702b73ec6dd3eb62d01d31b279c
Haemish Kyd <haemish.kyd@gmail.com>
2018-05-29 18:55:51 +0200

    First working version. Tests for robustness need to still be carried out.

--- merchant_guide.py
+++ merchant_guide.py
@@ -1,3 +1,5 @@
+import sys
+
 roman_numeral_dictionary = {
     "I":1,
     "V":5,
@@ -20,13 +22,19 @@ credits_per_piece = {
 
 questions_to_answer = []
 
+global_script_verbosity = 0
+
+def smart_print(string_to_print, string_verbosity_level):
+    if (string_verbosity_level <= global_script_verbosity):
+        print (string_to_print)
+
 
 def decode_the_roman_numeral_string(roman_numeral_string):
     maintained_total = 0
     preceding_is_part_of_num = False
 
     the_full_string_length = len(roman_numeral_string)
-    print ("String Length = "+str(the_full_string_length))
+    smart_print(("String Length = "+str(the_full_string_length)),1)
     for rn_idx in reversed(range(0, the_full_string_length)):
         if (preceding_is_part_of_num):
             preceding_is_part_of_num = False
@@ -42,9 +50,9 @@ def decode_the_roman_numeral_string(roman_numeral_string):
         else:
             current_value = roman_numeral_dictionary[roman_numeral_string[rn_idx]]
         maintained_total = maintained_total + current_value
-        print (current_value)
-    print("================")
-    print (maintained_total)
+        smart_print (current_value,1)
+    smart_print("================",1)
+    smart_print(maintained_total,1)
     return maintained_total
 
 
@@ -53,35 +61,94 @@ def read_the_file(the_file):
     for line in fh:
         # prepare the line the parsing
         processed_line = line.strip()
-        the_words = processed_line.split()        
-        # check which part of the file this is
+        the_words = processed_line.split()
+        #-----------------------------------------------------------------------------------------------------------
+        # this is a comment
+        #-----------------------------------------------------------------------------------------------------------
+        if the_words[0].startswith("#"):
+            continue        
+        #-----------------------------------------------------------------------------------------------------------
+        # if the sentence has 3 words and ends with a roman numeral - its a mapping to roman numerals
+        #-----------------------------------------------------------------------------------------------------------
         if ((len(the_words) == 3) and (processed_line[len(processed_line)-1] in roman_numeral_dictionary.keys())):
             # This is the intergalactic to roman numeral mapping
             # so push to that dictionary
             intergalactic_to_roman[the_words[0]] = the_words[2]
-        if ((the_words[0].lower() == "how") and (the_words[len(the_words)-1].endswith('?'))):
+            continue
+        #-----------------------------------------------------------------------------------------------------------
+        # if the sentence ends with "Credits" and has a number before it, starts with a intergalactic currency and 
+        # has an "is" before the number - then this is a rate 
+        #-----------------------------------------------------------------------------------------------------------
+        if ((the_words[len(the_words)-1] == "Credits") and (the_words[len(the_words)-2].isnumeric())
+            and (the_words[0] in intergalactic_to_roman.keys()) and (the_words[len(the_words)-3] == "is")):
+            # This is a rate statement
+            rate_amount = ""
+            for each_word in the_words:
+                if each_word in intergalactic_to_roman.keys():
+                    rate_amount = rate_amount + intergalactic_to_roman[each_word]
+            mineral_dust = the_words[len(the_words)-4]
+            credits_per_piece[mineral_dust] = (int(the_words[len(the_words)-2])/decode_the_roman_numeral_string(rate_amount))
+            continue
+        #-----------------------------------------------------------------------------------------------------------
+        # if the sentence starts with how many/much and ends with a question mark - its a question we need to answer
+        #-----------------------------------------------------------------------------------------------------------
+        if ((the_words[0].lower() == "how") and ((the_words[1].lower() == "many") or (the_words[1].lower() == "much"))
+            and (the_words[len(the_words)-1].endswith('?'))):
             # This is a question that needs to be answered
             questions_to_answer.append(processed_line)
+            continue
+            
+        smart_print("I'm sorry but \""+line+"\" doesn't make sense to me!",0)
+        
+
+def are_any_of_these_in_those(these, those):
+    for one_of_these in these:
+        if (one_of_these in those):
+            return True
+    return False
+
+
+def which_of_these_in_those(these, those):
+    for one_of_these in these:
+        if (one_of_these in those):
+            return one_of_these
+    return False
 
 
 def process_the_questions():
     for question in questions_to_answer:
-        # check if this has any mention of the commodities
-        for key in credits_per_piece.keys():
-            elements_of_sentence = question.replace('?','').split()
-            if key not in elements_of_sentence:
-                # This is a basic question 
-                roman_numeral_sentence = ""
-                for item in elements_of_sentence:
-                    if item in intergalactic_to_roman:
-                        roman_numeral_sentence= roman_numeral_sentence + intergalactic_to_roman[item]
-                print(decode_the_roman_numeral_string(roman_numeral_sentence))
-                break
+        # Is it a general question or is it a commodities based question
+        the_words = question.replace('?','').split()
+        intergalactic_sentence = ""
+        roman_numeral_sentence = ""
+        # Build up the intergalactic and roman numeral strings
+        for item in the_words:
+            if item in intergalactic_to_roman:
+                intergalactic_sentence = intergalactic_sentence + item + " "
+                roman_numeral_sentence = roman_numeral_sentence + intergalactic_to_roman[item]
+        if (len(intergalactic_sentence) == 0):
+            smart_print("I'm sorry but \""+question+"\" doesn't make sense to me!",0)
+            continue
+        if (("Credits" in the_words) and (are_any_of_these_in_those(credits_per_piece.keys(),the_words))):
+            smart_print((question+":"+"CQ"),1)            
+            current_commodity = which_of_these_in_those(credits_per_piece.keys(),the_words)
+            smart_print((intergalactic_sentence + current_commodity + " is " + str(decode_the_roman_numeral_string(roman_numeral_sentence)*credits_per_piece[current_commodity])),0)
+        else:
+            smart_print((question+":"+"NQ"),1)            
+            smart_print((intergalactic_sentence + " is " + str(decode_the_roman_numeral_string(roman_numeral_sentence))),0)
+            
 
-if __name__ == "__main__":    
-    print ("Welcome to the Merchant's Guid To the Galaxy")
-    read_the_file("test1.txt")
-    print(questions_to_answer)
-    process_the_questions()
-    #decode_the_roman_numeral_string("XCVIII")
+if __name__ == "__main__":
+    file_name = ""    
+    if (len(sys.argv) == 3):
+        # we have passed in the test script and verbosity level
+        file_name = sys.argv[2]
+        global_script_verbosity = int(sys.argv[1])
+    if (len(sys.argv) == 2):
+        file_name = sys.argv[1]
+        global_script_verbosity = 0
+    smart_print ("Welcome to the Merchant's Guide To the Galaxy",0)
+
+    read_the_file(file_name)
+    process_the_questions()    
 
